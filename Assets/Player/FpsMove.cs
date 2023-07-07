6db97a00761e556c1de987fd40de4af8758e25d4
XpravdaX <74926737+XpravdaX@users.noreply.github.com>
2023-07-07 13:49:00 +0300

    Add files via upload

---
+++ Assets/Player/FpsMove.cs
@@ -0,0 +1,142 @@
+﻿// автор Pravda_Sempai xd
+// https://www.youtube.com/channel/UCGHBUHxuZL9v8hEguNZkk7g
+using System.Collections;
+using UnityEngine;
+
+public class FpsMove : MonoBehaviour
+{
+    // Объявляем переменные внутри класса
+    [Space(20)] public Rigidbody rb; // Переменная Rigidbody, на которую будет действовать физическая сила
+    public float walkSpeed = 5f; // Скорость передвижения персонажа при ходьбе
+    Vector3 playerInput; // Вектор для хранения управляющих нажатий
+    Vector3 velocity; // Текущая скорость персонажа
+    Vector3 velocityChange; // Разница между желаемой и текущей скоростью персонажа
+
+    [Space(20)]
+    public bool enableJump = true; // Переменная, разрешающая прыжки
+    public float jumpPower = 5f; // Сила прыжка
+    public float crouchScale = 0.6f;
+    public KeyCode jumpKey = KeyCode.Space; // Кнопка прыжка
+    public KeyCode runKey = KeyCode.LeftShift; // Кнопка бега
+    public KeyCode corchKey = KeyCode.LeftControl; // Кнопка присесть
+
+    [Space(20)]
+    public AudioClip walkSound; // Звук ходьбы
+    public AudioClip crouchSound; // Звук приседания
+    public AudioClip runSound; // Звук бега
+
+    public AudioSource audioSource; // Компонент AudioSource для воспроизведения звуков
+
+
+    private bool isJumping; // Флаг, что персонаж прыгает
+    private bool isGrounded; // Флаг, что персонаж на земле
+
+    // Обновление состояния объекта каждый кадр
+    private void Update()
+    {
+        // Если разрешены прыжки, нажата кнопка прыжка и персонаж на земле, то выполняется прыжок
+        if (enableJump && Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
+
+        // Проверяем, на земле ли персонаж
+        CheckGround();
+    }
+
+    // Метод, вызывающийся каждый фиксированный кадр
+    void FixedUpdate()
+    {
+        // Скорость передвижения персонажа равна скорости ходьбы
+        float moveSpeed = walkSpeed;
+        if (playerInput != Vector3.zero & isGrounded == true) // Если есть управляющие нажатия
+        {
+            // Если нажата кнопка бега, то скорость удваивается
+            if (Input.GetKey(runKey))
+            {
+                moveSpeed *= 2f;
+                if (!audioSource.isPlaying || audioSource.clip != runSound) // Если звук бега не играет или звук ходьбы играет
+                {
+                    audioSource.clip = runSound;
+                    audioSource.Play();
+                }
+            }
+            else // Если не нажата кнопка бега
+            {
+                if (!audioSource.isPlaying || audioSource.clip !=crouchSound & audioSource.clip != walkSound) // Если звук ходьбы не играет или звук бега играет
+                {
+                    audioSource.clip = walkSound;
+                    audioSource.Play();
+                }
+            }
+
+            if (Input.GetKey(corchKey))
+            {
+                transform.localScale = new Vector3(1f, crouchScale, 1f);
+                moveSpeed -= 4f;
+
+                if (!audioSource.isPlaying || audioSource.clip != crouchSound) // Если звук бега не играет или звук ходьбы играет
+                {
+                    audioSource.clip = crouchSound;
+                    audioSource.Play();
+                }
+            }
+            else
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+        else // Если нет управляющих нажатий
+        {
+            audioSource.Stop(); // Останавливаем воспроизведение звуков
+        }
+
+        // Получаем управляющие нажатия и преобразуем вектор в соответствии с ориентацией персонажа
+        playerInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        playerInput = transform.TransformDirection(playerInput) * moveSpeed;
+
+        // Получаем текущую скорость персонажа и вычисляем разницу
+        velocity = rb.velocity;
+        velocityChange = (playerInput - velocity);
+        // При этом зануляем изменение по оси Y, чтобы персонаж не подпрыгивал вместе с движением
+        velocityChange.y = 0;
+
+        // Добавляем силу в соответствии с изменением скорости
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+
+    // Проверяем землю и обновляем соответствующие флаги
+    private void CheckGround()
+    {
+        // Определяем точку, в которой должна находиться нижняя точка персонажа 
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
+        // Определяем направление луча, который будет определять наличие земли
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+        // Максимальная дистанция луча определяется константой 
+        float distance = .75f;
+
+        // Если луч пересекается с коллайдером земли, то персонаж на земле
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, distance))
+        {
+            Debug.DrawRay(origin, direction * distance, Color.red); // Рисуем луч для отладки
+            isGrounded = true;
+            isJumping = false;
+        }
+        else
+        {
+            isGrounded = false;
+            isJumping = true;
+        }
+    }
+
+    // Метод выполнения прыжка
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            // Если персонаж на земле, то добавляем физическую силу для прыжка
+            rb.AddForce(0f, jumpPower, 0f, ForceMode.Impulse);
+            isGrounded = false; // Сбрасываем флаг нахождения на земле
+        }
+    }
+}
---
+++ Assets/Player/MouseRotator.cs
@@ -0,0 +1,60 @@
+﻿// скрипт для вращения камеры вокруг игрока в зависимости от движения мыши
+// автор Pravda xd
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotator : MonoBehaviour
+{
+
+    // переменные для хранения позиции мыши на оси X и Y
+    private float mouseX;
+    private float mouseY;
+
+    // настройка чувствительности мыши
+    [Range(50f, 500f)]
+    public float sentMouse = 200f;
+
+    // ограничения на вращение по оси Y
+    [Range(-90f, 90f)]
+    public float minYAngle = -90f;
+    [Range(-90f, 90f)]
+    public float maxYAngle = 90f;
+
+    // объект игрока для вращения вокруг него камеры
+    public Transform player;
+
+    // текущий угол вращения по осям X и Y
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+
+    private void Start()
+    {
+        // закрепляем курсор в центре экрана
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        // получаем значения движения мыши по осям X и Y и умножаем на чувствительность и время между кадрами
+        mouseX = Input.GetAxis("Mouse X") * sentMouse * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sentMouse * Time.deltaTime;
+
+        // вращение вокруг оси X
+        // уменьшаем угол вращения по оси X на значение движения мыши по Y (ускорение камеры вверх и вниз)
+        rotationX -= mouseY;
+        // ограничиваем угол вращения по оси Y с помощью функции Mathf.Clamp
+        rotationX = Mathf.Clamp(rotationX, minYAngle, maxYAngle);
+
+        // вращение вокруг оси Y
+        // увеличиваем угол вращения по оси Y на значение движения мыши по X (поворот камеры вокруг игрока)
+        rotationY += mouseX;
+
+        // применяем полученные углы вращения к повороту камеры
+        // вращение по оси X
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        // вращение по оси Y
+        player.rotation = Quaternion.Euler(0f, rotationY, 0f);
+    }
+}
